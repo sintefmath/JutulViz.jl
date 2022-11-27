83a18da01bfcc60c6a9d0ce9abe4bd8dddc32bfc
Olav Møyner <olav.moyner@gmail.com>
2022-11-27 22:56:47 +0100

    Update variables.jl

--- src/variables.jl
+++ src/variables.jl
@@ -3,11 +3,11 @@ function plot_secondary_variables(model::SimulationModel)
     plot_secondary_variables(MultiModel((model = model, )))
 end
 
-function plot_secondary_variables(model::MultiModel; resolution = default_jutul_resolution(), kwarg...)
+function plot_secondary_variables(model::MultiModel; resolution = default_jutul_resolution(), linewidth = 4, kwarg...)
     data = Dict{String, Any}()
     for (k, m) in pairs(model.models)
         for (vname, var) in Jutul.get_secondary_variables(m)
-            d = JutulLinePlotData(var)
+            d = line_plot_data(m, var)
             if !isnothing(d)
                 if d isa JutulLinePlotData
                     d = [d]
@@ -17,23 +17,28 @@ function plot_secondary_variables(model::MultiModel; resolution = default_jutul_
         end
     end
     fig = Figure(resolution = resolution)
-    ax = []
     dkeys = keys(data)
     default = first(dkeys)
     m = Menu(fig[1, 1], options = dkeys, default = default)
+    ax = Axis(fig[2, 1])
+    colors = Makie.wong_colors()
     # slider = Slider(fig[1:2, 2], range = 0.5:0.1:10, startvalue = 1, horizontal = false)
-
     function plot_var!(datakey)
-        d = data[datakey]
-        n = length(d)
-        plot_grid = fig[2, 1] = GridLayout(1, n)
-        for (i, di) in enumerate(d)
-            ax = Axis(plot_grid[1, i], xlabel = di.xlabel, ylabel = di.ylabel, title = di.title)
-            for (x, y, lbl) in zip(di.xdata, di.ydata, di.datalabels)
-                lines!(ax, x, y; label = lbl, kwarg...)
-            end
-            axislegend()
+        di = first(data[datakey])
+        empty!(ax)
+        ax.xlabel = di.xlabel
+        ax.ylabel = di.ylabel
+        ax.title = di.title
+            # ax = Axis(plot_grid[1, i], xlabel = di.xlabel, ylabel = di.ylabel, title = di.title)
+        labels_found = false
+        ix = 1
+        for (x, y, lbl) in zip(di.xdata, di.ydata, di.datalabels)
+            c = colors[mod(ix, 7) + 1]
+            lines!(ax, x, y; color = c, linewidth = linewidth, label = lbl, kwarg...)
+            # text!(ax, x[end], y[end], text = lbl)
+            ix += 1
         end
+        # axislegend()
     end
     on(m.selection) do datakey
         plot_var!(datakey)
